2858621c5c5b014464689ca833877ffc5ffcda4e
Víctor Hugo Campos Vázquez <vcamposvazquez@gmail.com>
2022-04-17 12:21:32 +0200

    make it better

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? 'https://dec4front2.herokuapp.com'
-		: 'http://localhost:3000';
+		? "https://back4de.herokuapp.com"
+		: "http://localhost:3000";
 
 export default baseUrl;
